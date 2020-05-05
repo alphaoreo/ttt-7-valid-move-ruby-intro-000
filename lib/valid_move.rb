1fd8f2425d5c0a00a1a59b2e137572c9948bd079
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-05-05 07:11:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index)
+  if !(index.between(0,board.length()))
     return false
-  elsif !(index.between(0,board.length()))
+  elsif position_taken?(board, index)
     return false
   else
     return true
